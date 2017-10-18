6500ac109680dcf66614aec6a72a208ffc937aff
Marie Kerkstoel <mariekerkstoel@gmail.com>
2017-10-18 15:31:24 +0100

    More specifications of the test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,19 @@
 require "fizzbuzz"
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+  it "returns 'Fizz' when passed 3" do
+    expect(fizzbuzz(3)).to eq 'Fizz'
+  end
+
+  it "returns 'Buzz' when passed 5" do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
+
+  it "returns 'Fizzbuzz' when passed 15" do
+    expect(fizzbuzz(15)).to eq "Fizzbuzz"
+  end
+
+  it "returns '7' when passed 7" do
+    expect(fizzbuzz(7)).to eq 7
   end
 end
